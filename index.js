eb51e79ed5a94108ee6591567678bbdff79e6e02
Trac-Duy Chu-Le <tracduy750@gmail.com>
2020-11-05 00:00:34 -0500

    making JS a function, calling to it in html

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-Array.prototype.forEach.call(document.getElementsByClassName('flip-card'), (flipCard) => {
-    const image = flipCard.getElementsByClassName('image')[0]
-    const description = flipCard.getElementsByClassName('description')[0]
-    const handleFlippedToBack = () => {
-      flipCard.className = 'flip-card flipped'
-      description.className = 'description front'
-      image.className = 'image back opacity-0'
-    }
-    const handleFlippedToFront = () => {
-      flipCard.className = 'flip-card'
-      image.className = 'image front'
-      description.className = 'description back opacity-0'
-    }
-    flipCard.addEventListener('click', () => {
-      if (flipCard.className.includes('flipped')) {
-        flipCard.removeEventListener('animationend', handleFlippedToBack)
-        flipCard.addEventListener('animationend', handleFlippedToFront)
-        flipCard.className = `${flipCard.className} flip-to-front`
-        description.className = `${description.className} flip-front`
-        image.className = 'image back'
-      } else {
-        flipCard.removeEventListener('animationend', handleFlippedToFront)
-        flipCard.addEventListener('animationend', handleFlippedToBack)
-        flipCard.className = `${flipCard.className} flip-to-back`
-        image.className = `${image.className} flip-front`
-        description.className = 'description back'
-      }
-    })
-  })
-  
+function setupFlipCards(){
+    Array.prototype.forEach.call(document.getElementsByClassName('flip-card'), (flipCard) => {
+        const image = flipCard.getElementsByClassName('image')[0]
+        const description = flipCard.getElementsByClassName('description')[0]
+        const handleFlippedToBack = () => {
+          flipCard.className = 'flip-card flipped'
+          description.className = 'description front'
+          image.className = 'image back opacity-0'
+        }
+        const handleFlippedToFront = () => {
+          flipCard.className = 'flip-card'
+          image.className = 'image front'
+          description.className = 'description back opacity-0'
+        }
+        flipCard.addEventListener('click', () => {
+          if (flipCard.className.includes('flipped')) {
+            flipCard.removeEventListener('animationend', handleFlippedToBack)
+            flipCard.addEventListener('animationend', handleFlippedToFront)
+            flipCard.className = `${flipCard.className} flip-to-front`
+            description.className = `${description.className} flip-front`
+            image.className = 'image back'
+          } else {
+            flipCard.removeEventListener('animationend', handleFlippedToFront)
+            flipCard.addEventListener('animationend', handleFlippedToBack)
+            flipCard.className = `${flipCard.className} flip-to-back`
+            image.className = `${image.className} flip-front`
+            description.className = 'description back'
+          }
+        })
+      })
+}
